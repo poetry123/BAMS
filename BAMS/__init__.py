03929980617d9aaca165f388a68e283194214fd4
Yuan <yw.johnny@gmail.com>
2017-04-08 16:15:53 +0100

    Add Bootstrap Admin Template

--- BAMS/__init__.py
+++ BAMS/__init__.py
@@ -8,5 +8,8 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('quotation_new', '/quotation/new')
+    config.add_route('quotation_create', '/quotation/create')
+    config.add_route('quotation_edit', '/quotation/edit/{quotation_no}')
     config.scan()
     return config.make_wsgi_app()
--- BAMS/views.py
+++ BAMS/views.py
@@ -1,6 +1,29 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/quotation_list.pt')
 def my_view(request):
-    return {'project': 'BAMS'}
+    # get all quotations from database
+    quotations = []
+    for i in range(5):
+      quotation = {'quotation_no': (i+1)*1000}
+      quotations.append(quotation)
+    return {'quotations': quotations}
+
+@view_config(route_name='quotation_new', renderer='templates/new_form.pt')
+def quotation_new(request):
+  return {}
+
+@view_config(route_name='quotation_edit', renderer='templates/edit_form.pt')
+def quotation_edit(request):
+  quotation_no = request.matchdict['quotation_no']
+  # get entry from mongodb based on quotation number
+  return {'qt_number': quotation_no}
+
+@view_config(route_name='quotation_create')
+def create_quotation(request):
+  if request.method == 'POST':
+    quotation = request.POST
+    print(quotation)
+    # Save to mongodb
+    return HTTPFound(location='/')
